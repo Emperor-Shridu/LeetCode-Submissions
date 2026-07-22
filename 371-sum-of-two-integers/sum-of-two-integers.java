55b129f99d67f0ffd55be90c4b560de2451caabf
Emperor-Shridu <68996437+Emperor-Shridu@users.noreply.github.com>
2026-07-22 18:07:03 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (99.28%) - LeetSync

---
+++ 371-sum-of-two-integers/sum-of-two-integers.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int getSum(int a, int b) {
+        int carry;
+        while(b!=0){
+            carry = (a&b)<<1;
+            a = a^b;
+            b = carry;
+        }
+        return a;
+    }
+}
